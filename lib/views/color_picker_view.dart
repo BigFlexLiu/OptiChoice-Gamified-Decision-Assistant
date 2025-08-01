cb967248b2234ccfe97d18af3b788250cf37adaf
felix <felixliu135@gmail.com>
2025-08-01 17:22:45 -0400

    Enhancement: color wheel UI

--- lib/views/color_picker_view.dart
+++ lib/views/color_picker_view.dart
@@ -6,6 +6,14 @@ import 'dart:typed_data';
 import 'package:flutter_reorderable_grid_view/widgets/widgets.dart';
 import '../widgets/dialogs/unsaved_changes_dialog.dart';
 
+// Constants
+const int _kMaxSelectedColors = 12;
+const int _kMinSelectedColors = 2;
+const double _kColorWheelMargin = 96.0;
+const double _kMinColorWheelSize = 200.0;
+const int _kColorWheelResolution = 400;
+const double _kHueConversionFactor = 57.2958;
+
 class ColorPickerView extends StatefulWidget {
   final List<Color> initialColors;
   final Function(List<Color>) onColorsChanged;
@@ -22,7 +30,7 @@ class ColorPickerView extends StatefulWidget {
 
 class _ColorPickerViewState extends State<ColorPickerView> {
   List<Color> selectedColors = [];
-  Color? currentColor;
+  Color currentColor = Color.from(alpha: 255, red: 255, green: 255, blue: 255);
   final ScrollController _scrollController = ScrollController();
   late List<Color> _initialColors;
 
@@ -72,17 +80,17 @@ class _ColorPickerViewState extends State<ColorPickerView> {
   }
 
   void _addCurrentColorToSelected() {
-    if (currentColor != null && selectedColors.length < 12) {
+    if (selectedColors.length < _kMaxSelectedColors) {
       setState(() {
-        if (!selectedColors.contains(currentColor!)) {
-          selectedColors.add(currentColor!);
+        if (!selectedColors.contains(currentColor)) {
+          selectedColors.add(currentColor);
         }
       });
     }
   }
 
   void _removeSelectedColor(Color color) {
-    if (selectedColors.length > 2) {
+    if (selectedColors.length > _kMinSelectedColors) {
       setState(() {
         selectedColors.remove(color);
       });
@@ -142,101 +150,133 @@ class _ColorPickerViewState extends State<ColorPickerView> {
         ),
         body: Column(
           children: [
-            Expanded(
-              flex: 3,
-              child: Center(
-                child: ColorWheel(onColorSelected: _setCurrentColor),
-              ),
+            _buildColorWheelSection(),
+            _buildColorPreviewSection(),
+            _buildSelectedColorsSection(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColorWheelSection() {
+    return Expanded(
+      flex: 3,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final availableWidth = MediaQuery.of(context).size.width;
+          final availableHeight = constraints.maxHeight;
+          final maxSize =
+              math.min(availableWidth, availableHeight) - _kColorWheelMargin;
+          final wheelSize = math.max(_kMinColorWheelSize, maxSize.toDouble());
+
+          return Center(
+            child: ColorWheel(
+              onColorSelected: _setCurrentColor,
+              size: wheelSize,
             ),
-            if (currentColor != null)
-              GestureDetector(
-                onTap: selectedColors.length < 12
-                    ? _addCurrentColorToSelected
-                    : null,
-                child: Container(
-                  height: 60,
-                  width: double.infinity,
-                  margin: const EdgeInsets.all(16),
-                  color: selectedColors.length < 12
-                      ? currentColor
-                      : Colors.grey.shade400,
-                  child: Center(
-                    child: Text(
-                      selectedColors.length < 12
-                          ? 'Tap to Add Color (${selectedColors.length}/12)'
-                          : 'Maximum 12 colors reached',
-                      style: TextStyle(
-                        color: selectedColors.length < 12
-                            ? (currentColor!.computeLuminance() > 0.5
-                                  ? Colors.black
-                                  : Colors.white)
-                            : Colors.black54,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildColorPreviewSection() {
+    return GestureDetector(
+      onTap: selectedColors.length < _kMaxSelectedColors
+          ? _addCurrentColorToSelected
+          : null,
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        margin: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: selectedColors.length < _kMaxSelectedColors
+              ? currentColor
+              : Colors.grey.shade400,
+          border: Border.all(color: Colors.grey.shade300, width: 2),
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            selectedColors.length < _kMaxSelectedColors
+                ? 'Tap to Add Color (${selectedColors.length}/$_kMaxSelectedColors)'
+                : 'Maximum $_kMaxSelectedColors colors reached',
+            style: TextStyle(
+              color: selectedColors.length < _kMaxSelectedColors
+                  ? (currentColor.computeLuminance() > 0.5
+                        ? Colors.black
+                        : Colors.white)
+                  : Colors.black54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSelectedColorsSection() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 8),
             Expanded(
-              flex: 1,
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 8),
-                    Expanded(
-                      child: ReorderableBuilder(
-                        scrollController: _scrollController,
-                        onReorder: _reorderColors,
-                        fadeInDuration: const Duration(milliseconds: 100),
-                        releasedChildDuration: const Duration(milliseconds: 0),
-                        positionDuration: const Duration(microseconds: 0),
-                        builder: (children) {
-                          return GridView(
-                            controller: _scrollController,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 6,
-                                  crossAxisSpacing: 8,
-                                  mainAxisSpacing: 8,
-                                  childAspectRatio: 1,
-                                ),
-                            children: children,
-                          );
-                        },
-                        children: List.generate(selectedColors.length, (index) {
-                          final color = selectedColors[index];
-                          return GestureDetector(
-                            key: Key('color_$index'),
-                            onTap: selectedColors.length > 2
-                                ? () => _removeSelectedColor(color)
-                                : null,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: color,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(
-                                  color: Colors.grey.shade300,
-                                  width: 2,
-                                ),
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.close,
-                                  color: selectedColors.length > 2
-                                      ? Colors.white70
-                                      : Colors.white30,
-                                  size: 20,
-                                ),
-                              ),
-                            ),
-                          );
-                        }),
+              child: ReorderableBuilder(
+                scrollController: _scrollController,
+                onReorder: _reorderColors,
+                fadeInDuration: const Duration(milliseconds: 100),
+                releasedChildDuration: const Duration(milliseconds: 0),
+                positionDuration: const Duration(microseconds: 0),
+                builder: (children) => GridView(
+                  controller: _scrollController,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 6,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                    childAspectRatio: 1,
+                  ),
+                  children: children,
+                ),
+                children: List.generate(selectedColors.length, (index) {
+                  final color = selectedColors[index];
+                  return GestureDetector(
+                    key: Key('color_$index'),
+                    onTap: selectedColors.length > _kMinSelectedColors
+                        ? () => _removeSelectedColor(color)
+                        : null,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: color,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 2,
+                        ),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.close,
+                          color: selectedColors.length > _kMinSelectedColors
+                              ? Colors.white70
+                              : Colors.white30,
+                          size: 20,
+                        ),
                       ),
                     ),
-                  ],
-                ),
+                  );
+                }),
               ),
             ),
           ],
@@ -246,10 +286,15 @@ class _ColorPickerViewState extends State<ColorPickerView> {
   }
 }
 
+/// A circular color wheel widget that displays the full color spectrum.
+///
+/// The wheel is generated once at high resolution and cached for performance.
+/// It adapts to different sizes by scaling the cached image.
 class ColorWheel extends StatefulWidget {
   final Function(Color) onColorSelected;
+  final double? size;
 
-  const ColorWheel({super.key, required this.onColorSelected});
+  const ColorWheel({super.key, required this.onColorSelected, this.size});
 
   @override
   State<ColorWheel> createState() => _ColorWheelState();
@@ -259,10 +304,17 @@ class _ColorWheelState extends State<ColorWheel> {
   static ui.Image? _globalCache;
   static Future<ui.Image>? _generating;
 
+  double get _wheelSize => widget.size ?? 300.0;
+
   @override
   void initState() {
     super.initState();
     _initCache();
+  }
+
+  @override
+  void didUpdateWidget(ColorWheel oldWidget) {
+    super.didUpdateWidget(oldWidget);
   }
 
   void _initCache() async {
@@ -277,10 +329,10 @@ class _ColorWheelState extends State<ColorWheel> {
   }
 
   static Future<ui.Image> _generateColorWheel() async {
-    const size = 300;
+    const size = _kColorWheelResolution;
     final pixels = Uint8List(size * size * 4);
     const center = size * 0.5;
-    const radius = size * 0.5;
+    const radius = center;
 
     for (int y = 0, i = 0; y < size; y++) {
       final dy = y - center;
@@ -289,15 +341,14 @@ class _ColorWheelState extends State<ColorWheel> {
         final distSq = dx * dx + dy * dy;
 
         if (distSq <= radius * radius) {
-          final angle = math.atan2(dy, dx);
-          final hue = (angle * 57.2958 + 360) % 360; // 180/π ≈ 57.2958
+          final hue = (math.atan2(dy, dx) * _kHueConversionFactor + 360) % 360;
           final sat = math.sqrt(distSq) / radius;
 
-          // Correct HSV to RGB using full value (brightness = 1.0)
+          // HSV to RGB conversion
           final h60 = hue / 60;
-          final c = sat; // chroma = saturation * value (where value = 1.0)
+          final c = sat;
           final x = c * (1 - (h60 % 2 - 1).abs());
-          final m = 1.0 - c; // m = value - chroma = 1.0 - sat
+          final m = 1.0 - c;
 
           final (r, g, b) = switch (h60.floor()) {
             0 => (c + m, x + m, m),
@@ -329,40 +380,47 @@ class _ColorWheelState extends State<ColorWheel> {
 
   @override
   void dispose() {
-    // Don't dispose the globally cached image, just clear local reference
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTapDown: (details) {
-        final RenderBox box = context.findRenderObject() as RenderBox;
-        final localPosition = box.globalToLocal(details.globalPosition);
-        final color = _getColorFromPosition(localPosition, box.size);
-        if (color != null) {
-          widget.onColorSelected(color);
-        }
-      },
-      onPanUpdate: (details) {
-        final RenderBox box = context.findRenderObject() as RenderBox;
-        final localPosition = box.globalToLocal(details.globalPosition);
-        final color = _getColorFromPosition(localPosition, box.size);
-        if (color != null) {
-          widget.onColorSelected(color);
-        }
-      },
-      child: _globalCache != null
-          ? CustomPaint(
-              size: const Size(300, 300),
-              painter: CachedColorWheelPainter(_globalCache!),
-            )
-          : const SizedBox(
-              width: 300,
-              height: 300,
-              child: Center(child: CircularProgressIndicator()),
+      onTapDown: _handleColorSelection,
+      onPanUpdate: _handleColorSelection,
+      child: Container(
+        width: _wheelSize,
+        height: _wheelSize,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(_globalCache != null ? 0.4 : 0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 4),
             ),
+          ],
+        ),
+        child: _globalCache != null
+            ? CustomPaint(
+                size: Size(_wheelSize, _wheelSize),
+                painter: CachedColorWheelPainter(_globalCache!),
+              )
+            : const Center(child: CircularProgressIndicator()),
+      ),
     );
+  }
+
+  void _handleColorSelection(details) {
+    final RenderBox box = context.findRenderObject() as RenderBox;
+    final localPosition = box.globalToLocal(details.globalPosition);
+    final color = _getColorFromPosition(
+      localPosition,
+      Size(_wheelSize, _wheelSize),
+    );
+    if (color != null) {
+      widget.onColorSelected(color);
+    }
   }
 
   Color? _getColorFromPosition(Offset position, Size size) {
@@ -373,7 +431,7 @@ class _ColorWheelState extends State<ColorWheel> {
 
     if (distSq > center * center) return null;
 
-    final hue = (math.atan2(dy, dx) * 57.2958 + 360) % 360;
+    final hue = (math.atan2(dy, dx) * _kHueConversionFactor + 360) % 360;
     final saturation = math.sqrt(distSq) / center;
 
     return HSVColor.fromAHSV(1.0, hue, saturation, 1.0).toColor();
@@ -387,12 +445,17 @@ class CachedColorWheelPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    // Simply draw the cached image
-    canvas.drawImage(cachedImage, Offset.zero, Paint());
+    final srcRect = Rect.fromLTWH(
+      0,
+      0,
+      cachedImage.width.toDouble(),
+      cachedImage.height.toDouble(),
+    );
+    final dstRect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawImageRect(cachedImage, srcRect, dstRect, Paint());
   }
 
   @override
-  bool shouldRepaint(CachedColorWheelPainter oldDelegate) {
-    return oldDelegate.cachedImage != cachedImage;
-  }
+  bool shouldRepaint(CachedColorWheelPainter oldDelegate) =>
+      oldDelegate.cachedImage != cachedImage;
 }
