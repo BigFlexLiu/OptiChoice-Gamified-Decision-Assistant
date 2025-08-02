39f33bba7b3402edf1cde4214a273581e066cabf
felix <felixliu135@gmail.com>
2025-08-02 11:26:37 -0400

    Enhancement: Lock screen in protrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:decision_spinner/views/spinner_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_soloud/flutter_soloud.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock orientation to portrait mode
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   // Initialize SoLoud audio engine
   await SoLoud.instance.init();
